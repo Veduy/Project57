0be8d6d5f230ab8799df6b8be2f0943b4a35f92a
DBCKS97 <wonyu1997@naver.com>
2025-12-31 15:19:39 +0900

    FBlueprintSessionResult
    Result.OnlineResult.Session.SessionSettings.Set(FName("SessionName"), FString(TEXT("1 vs 1 gosuman"), EOnlineDataAdvertisementType::ViaOnlineService));
    
    Result.OnlineResult.Session.SessionSettings.Get(FName("SessionName"), OUT SessionName);

--- Source/Project57/Session/SessionItemWidget.cpp
+++ Source/Project57/Session/SessionItemWidget.cpp
@@ -14,16 +14,31 @@ bool USessionItemWidget::Initialize()
 
 void USessionItemWidget::SetInfo(FBlueprintSessionResult InSessionResult)
 {
+    //Set(SETTING_MAPNAME, FString(TEXT("")), EOnlineDataAdvertisementType::ViaOnlineService);
+    //Set(SETTING_NUMBOTS, 0, EOnlineDataAdvertisementType::ViaOnlineService);
+    //Set(SETTING_GAMEMODE, FString(TEXT("")), EOnlineDataAdvertisementType::ViaOnlineService);
+
     Result = InSessionResult;
+    Result.OnlineResult.Session.SessionSettings.Set(FName("SessionName"), FString(TEXT("1 vs 1 gosuman"), EOnlineDataAdvertisementType::ViaOnlineService));
+    Result.OnlineResult.Session.SessionSettings.Set(FName("MapName"), FString(TEXT("firstMap"), EOnlineDataAdvertisementType::ViaOnlineService));
+
+    //Result.OnlineResult.Session.SessionSettings
+
     RefreshUI();
 }
 
 void USessionItemWidget::RefreshUI()
 {
-    Txt_SessionName->SetText(FText());
-    Txt_PlayerCount->SetText(FText());
-    Txt_MapName->SetText(FText());
+    FString SessionName;
+    Result.OnlineResult.Session.SessionSettings.Get(FName("SessionName"), OUT SessionName);
+    Txt_SessionName->SetText(FText::FromString(SessionName));
+ 
+    const int32 CurrentPlayers = Result.OnlineResult.Session.SessionSettings.NumPublicConnections - Result.OnlineResult.Session.NumOpenPublicConnections;
+    const int32 MaxPlayers = Result.OnlineResult.Session.SessionSettings.NumPublicConnections;
+    FString PlayerCount = FString::Printf(TEXT("(%d / %d)"), MaxPlayers, CurrentPlayers);
+    Txt_PlayerCount->SetText(FText::FromString(PlayerCount));
 
-
-
+    FString MapName;
+    Result.OnlineResult.Session.SessionSettings.Get(FName("MapName"), OUT MapName);
+    Txt_MapName->SetText(FText::FromString(MapName));
 }
